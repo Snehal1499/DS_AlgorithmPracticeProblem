34ec24d89acd2ef2c882c8493ae6b6b15ac32375
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-29 15:42:44 +0530

    Added Two String Functions Are Euqal

--- Day16_17Problems/Program.cs
+++ Day16_17Problems/Program.cs
@@ -7,6 +7,28 @@ namespace Day16_17Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Algorithm Programs");
+            Console.WriteLine("Algorithm Programs \n1)  String Equal are not Program");
+            StringEqual se = new StringEqual();
+            string str = "SNEHAL";
+            string str2 = "SNEHAL";
+            string str3 = "SNEHAL";
+            int s = str.Length;
+            int h = str2.Length;
+            int l = str3.Length;
+            StringEqual.permute(str, 0, s - 1);
+            Console.WriteLine("\n" + StringEqual.s + "\n");
+            StringEqual.s2 = StringEqual.s;
+            StringEqual.permute(str2, 0, h - 1);
+            Console.WriteLine("\n" + StringEqual.s);
+            StringEqual.s3 = StringEqual.s;
+            StringEqual.permute(str3, 0, l - 1);
+            Console.WriteLine("\n" + StringEqual.s + "\n");
+            StringEqual.s4 = StringEqual.s;
+            Console.WriteLine("Checking Lengths : True is if both strings are equal : Str and Str2 ");
+            Console.WriteLine(StringEqual.s2.Equals(StringEqual.s3));
+            Console.WriteLine("Checking Lengths : True is if both strings are equal : Str2 and Str3 ");
+            Console.WriteLine(StringEqual.s3.Equals(StringEqual.s4));
+            Console.WriteLine("-----------------------------------------------------------");
         }
     }
 }
---
+++ Day16_17Problems/StringEqual.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_17Problems
+{
+    internal class StringEqual
+    {
+        public static string s, s2, s3, s4;
+        public static void permute(string str, int l, int r)
+
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+
+        }
+
+        public static string swap(string a, int i, int j)
+
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            s = new string(charArray);
+            return s;
+
+        }
+
+
+    }
+}
+    
+
